831cc99cc1c48bbf4d3c37474e3fb44311dfa149
Abderrahmane Ahouari <abdorrahmane054@gmail.com>
2025-01-17 02:02:33 +0100

    finished working on the tags methods

--- pages/admin/categories.php
+++ pages/admin/categories.php
@@ -78,6 +78,7 @@
          </div>
       </div>
    <div class="flex items-center justify-center mb-4 rounded bg-gray-50 dark:bg-gray-800">
+      
     <table class="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
         <thead class="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
             <tr>
